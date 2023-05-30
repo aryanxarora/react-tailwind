791aff15487f8e731aab64726f96ac1ec0c5049b
aryanxarora <aryanxarora@gmail.com>
2023-05-30 19:10:29 -0400

    react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './styles/App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={} /> */}
+        </Routes>
+      </Router>      
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return <nav>Navbar</nav>;
+}
+
+export default Navbar;
